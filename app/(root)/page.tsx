193653eeed5825d410bba6a63827b5f470497f98
Pakrinha Sim <pakrinha124@gmail.com>
2025-10-01 23:22:30 -0700

    Implement the TradingViewWidget and useTradingViewWidget for the main page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import TradingViewWidget from "@/components/TradingViewWidget";
+import {MARKET_DATA_WIDGET_CONFIG} from "@/lib/constants";
 
 const Home = () => {
     return (
         <div className="flex min-h-screen home-wrapper">
-            Home
+            <section className="grid w-full gap-8 home section">
+                <div className = "md:col-span-1 cl:col-span-1">
+
+                <TradingViewWidget
+                title= "Market Overview"
+                scriptUrl= "https://s3.tradingview.com/external-embedding/embed-widget-market-overview.js"
+                config = {MARKET_DATA_WIDGET_CONFIG}
+                className= "custom-chart"
+                />
+                </div>
+            </section>
         </div>
     )
 }
